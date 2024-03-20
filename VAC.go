023b3afb97318fd72e9493972890e58dc9dddccc
Abhijith Ajith <113827354+AAbhijithA@users.noreply.github.com>
2024-03-20 16:08:08 +0530

    Update VAC.go

--- VAC.go
+++ VAC.go
@@ -8,17 +8,17 @@ import (
 func main(){
     var C int
     fmt.Scan(&C)
-	L := 0
-	Lc := 0
-	Ac := 1
-	for {
-	    Lc += Ac
-	    if Lc > C {
-	        break
-	    }
-	    C -= Lc
-	    Ac += 1
-	    L += 1
+    L := 0
+    Lc := 0
+    Ac := 1
+    for {
+	Lc += Ac
+	if Lc > C {
+	    break
 	}
-	fmt.Printf("%d",L)
+	C -= Lc
+	Ac += 1
+	L += 1
+    }
+    fmt.Printf("%d",L)
 }
